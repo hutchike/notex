cacf80f81240b178ce61ae03828e7dbaab3663d7
Kevin <kevin.hutchinson@legendum.com>
2010-07-28 14:26:41 -0500

    fixed a bug

--- app/public/scripts/notex.js
+++ app/public/scripts/notex.js
@@ -5,11 +5,11 @@ var notex = {
   selected: null,
   editing: false,
   notes: {},
-  count: 0,
   cursor: {x: null, y: null},
   offset: {x: 42, y: 114},
   adjust: {x: 0, y: 7},
   origin: {x: null, y: null},
+  nearby: {x: 10, y: 20},
   init: function() {
     $('#page').mousemove(function(e) {
       notex.cursor.x = e.pageX;
@@ -24,19 +24,29 @@ var notex = {
     if (typeof e.pageX == 'undefined') {
       notex.origin.y += notex.line_height;
       if (notex.origin.y > notex.page_height - notex.line_height) return;
-    } else if (notex.selected) {
-      var id = notex.selected.attr('id');
-      var note = notex.notes[id];
-      text = notex.selected.text(); notex.selected.text('');
-      notex.origin.x = note.x-1;
-      notex.origin.y = note.y-1;
-      notex.notes[id] = null;
+      notex.selected = null;
+      for (id in notex.notes) {
+        var note = notex.notes[id];
+        if (note && notex.is_near(note, notex.origin)) {
+          notex.selected = $('#'+note.id);
+        }
+      }
     } else {
       notex.origin.x = notex.cursor.x - notex.offset.x;
       notex.origin.y = notex.cursor.y - notex.offset.y - notex.adjust.y;
     }
+    if (notex.selected) {
+      var id = notex.selected.attr('id');
+      var note = notex.notes[id];
+      if (note) {
+        text = notex.selected.text(); notex.selected.text('');
+        notex.origin.x = note.x-1;
+        notex.origin.y = note.y-1;
+        delete notex.notes[id];
+      }
+    }
     var width = notex.page_width - notex.origin.x;
-    $('#edit').css({'top': notex.origin.y, 'left': notex.origin.x, 'width': width}).attr('value', text).show().focus();
+    $('#edit').css({'top': notex.origin.y, 'left': notex.origin.x, 'width': width}).attr('value', text).show().select().focus();
     notex.editing = true;
   },
   write: function(opts) {
@@ -49,7 +59,7 @@ var notex = {
   },
   create: function(text) {
     $('#edit').attr('value', '').hide();
-    var id = notex.count++;
+    var id = (new Date()).getTime();
     var note = {id: 'note'+id, x: notex.origin.x+1, y: notex.origin.y+1, text: text};
     notex.notes[note.id] = note;
     notex.render(note);
@@ -69,7 +79,6 @@ var notex = {
       for (id in notex.notes) {
         var note = notex.notes[id];
         if (note) notex.render(note);
-        notex.count++;
       }
     });
   },
@@ -78,5 +87,11 @@ var notex = {
     function(data) {
       // callback
     });
-  }
+  },
+  is_near: function(pos1, pos2) {
+    var x_diff = Math.abs(pos1.x - pos2.x);
+    var y_diff = Math.abs(pos1.y - pos2.y);
+    return (x_diff <= notex.nearby.x && y_diff <= notex.nearby.y ? true : false);
+  },
+  debug: function(obj) { $('#debug').text('['+$.toJSON(obj)+']') }
 };
