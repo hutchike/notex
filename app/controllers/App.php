ee9b48c9264c75fac7ad1022322f3a3441592760
Your Name <kevin.hutchinson@legendum.com>
2010-07-29 00:14:01 -0500

    Total package

--- app/controllers/App.php
+++ app/controllers/App.php
@@ -5,24 +5,10 @@ class App_controller extends Controller
 {
     public function before()
     {
-        $this->render->title = 'the web notepad';
+        $uri = $_SERVER['REQUEST_URI'];
+        $this->render->title = $uri == '/' ? 'the web notepad' : ltrim($uri, '/');
         $this->render->debug = '';
         $this->render->layout = 'notepad';
-
-        // Redirect to a unique notepad if not chosen
-
-        $request = $_SERVER['REQUEST_URI'];
-        if ($request == '/')
-        {
-            $md5 = md5($_SERVER['REMOTE_ADDR'] . time());
-            $url = '/' . substr($md5, 0, 10);
-            header("Location: $url");
-            exit;
-        }
-        else // good, we've chosen a personal notepad
-        {
-            $this->render->title = ltrim($request, '/');
-        }
     }
 }
 
