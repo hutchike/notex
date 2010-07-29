b79086c793f73fa9da088af842f436b56195ebe1
Your Name <kevin.hutchinson@legendum.com>
2010-07-29 01:08:45 -0500

    Added a copyright message

--- app/views/en/types/html.php
+++ app/views/en/types/html.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"> 
 <head> 
+<!--
+ | noteX - Copyright (c) 2010 Kevin Hutchinson <hutchike@gmail.com>
+ |         All Rights Reserved. Please contact me if you'd like to
+ |         own a copy of this system or have it installed locally.
+ +-->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="/styles/notex.css" />
     <script type="text/javascript" src="/scripts/jquery/all.js"></script>
