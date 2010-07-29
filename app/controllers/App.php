fa3bd83f9b294c3e2206a452ab629e24a2b9240c
Your Name <kevin.hutchinson@legendum.com>
2010-07-29 00:24:19 -0500

    Better title

--- app/controllers/App.php
+++ app/controllers/App.php
@@ -6,7 +6,7 @@ class App_controller extends Controller
     public function before()
     {
         $uri = $_SERVER['REQUEST_URI'];
-        $this->render->title = $uri == '/' ? 'the web notepad' : ltrim($uri, '/');
+        $this->render->title = $uri == '/' ? 'your web notepad' : ltrim($uri, '/');
         $this->render->debug = '';
         $this->render->layout = 'notepad';
     }
