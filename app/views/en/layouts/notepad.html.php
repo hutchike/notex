e1bd73430ce6cac2ff3e4df45b61a185a34d10a7
Your Name <kevin.hutchinson@legendum.com>
2010-07-31 15:27:19 -0500

    Added a Twitter login

--- app/views/en/layouts/notepad.html.php
+++ app/views/en/layouts/notepad.html.php
@@ -1,3 +1,8 @@
+<? if (is_null($auth_token)): ?>
+<div id="twitter">
+  <a href="/twitter/login"><img src="/images/twitter/lighter.png" alt="twitter login"/></a>
+</div>
+<? endif ?>
 <div id="notepad">
   <div id="page">
     <div id="content"><?= $content ?></div>
