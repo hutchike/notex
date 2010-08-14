f68627063b178d5aaa95f7d090c69d96049a5c3e
Your Name <kevin.hutchinson@legendum.com>
2010-08-14 18:28:52 -0500

    easier to see the pen style now

--- app/views/en/layouts/notepad.html.php
+++ app/views/en/layouts/notepad.html.php
@@ -19,19 +19,19 @@
   <div id="selectfont"></div>
   <img src="/images/penbox.png" id="styleimg" width="200" height="82" usemap="#penmap" alt="penbox" />
   <map name="penmap">
-    <area shape="rect" coords="4,28,27,52" onclick="notex.penbox.set_color('black', 4, 28)" alt="black" />
-    <area shape="rect" coords="28,28,51,52" onclick="notex.penbox.set_color('#333', 28, 28)" alt="slate gray" />
-    <area shape="rect" coords="52,28,75,52" onclick="notex.penbox.set_color('#666', 52, 28)" alt="steel gray" />
-    <area shape="rect" coords="76,28,99,52" onclick="notex.penbox.set_color('#80007e', 76, 28)" alt="deep purple" />
-    <area shape="rect" coords="100,28,123,52" onclick="notex.penbox.set_color('#008001', 100, 28)" alt="forest green" />
-    <area shape="rect" coords="123,28,146,52" onclick="notex.penbox.set_color('#0003ff', 123, 28)" alt="aqua blue" />
-    <area shape="rect" coords="147,28,170,52" onclick="notex.penbox.set_color('#ff00fc', 147, 28)" alt="pink kiss" />
-    <area shape="rect" coords="171,28,194,52" onclick="notex.penbox.set_color('red', 171, 28)" alt="red alert" />
+    <area shape="rect" coords="4,29,27,52" onclick="notex.penbox.set_color('black', 4, 28)" alt="black" />
+    <area shape="rect" coords="28,29,51,52" onclick="notex.penbox.set_color('#333', 28, 28)" alt="slate gray" />
+    <area shape="rect" coords="52,29,75,52" onclick="notex.penbox.set_color('#666', 52, 28)" alt="steel gray" />
+    <area shape="rect" coords="76,29,99,52" onclick="notex.penbox.set_color('#80007e', 76, 28)" alt="deep purple" />
+    <area shape="rect" coords="100,29,123,52" onclick="notex.penbox.set_color('#008001', 100, 28)" alt="forest green" />
+    <area shape="rect" coords="123,29,146,52" onclick="notex.penbox.set_color('#0003ff', 123, 28)" alt="aqua blue" />
+    <area shape="rect" coords="147,29,170,52" onclick="notex.penbox.set_color('#ff00fc', 147, 28)" alt="pink kiss" />
+    <area shape="rect" coords="171,29,194,52" onclick="notex.penbox.set_color('red', 171, 28)" alt="red alert" />
 
-    <area shape="rect" coords="4,56,51,78" onclick="notex.penbox.set_font('sans', 4, 56)" alt="sans serif" />
-    <area shape="rect" coords="53,56,100,78" onclick="notex.penbox.set_font('serif', 53, 56)" alt="serif" />
-    <area shape="rect" coords="101,56,147,78" onclick="notex.penbox.set_font('mono', 101, 56)" alt="mono" />
-    <area shape="rect" coords="149,56,196,78" onclick="notex.penbox.set_font('script', 149, 56)" alt="script" />
+    <area shape="rect" coords="4,56,51,78" onclick="notex.penbox.set_font('sans', 4, 55)" alt="sans serif" />
+    <area shape="rect" coords="53,56,100,78" onclick="notex.penbox.set_font('serif', 53, 55)" alt="serif" />
+    <area shape="rect" coords="101,56,147,78" onclick="notex.penbox.set_font('mono', 101, 55)" alt="mono" />
+    <area shape="rect" coords="149,56,196,78" onclick="notex.penbox.set_font('script', 149, 55)" alt="script" />
   </map>
 </div>
 <div id="notebox">
@@ -39,7 +39,7 @@
   <div id="paper" onclick="alert('paper')"><img src="/images/nothing.gif" alt="paper" /></div>
   <div id="canread" onclick="alert('canread')"></div>
   <div id="canedit" onclick="alert('canedit')"></div>
-  <img src="/images/notebox.png" id="styleimg" width="200" height="130" usemap="#notemap" alt="notebox" />
+  <img src="/images/notebox.png" id="styleimg" width="200" height="138" usemap="#notemap" alt="notebox" />
   <map name="notemap">
   </map>
 </div>
